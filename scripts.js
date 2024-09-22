fa90418ad7fe19a3236e93040e9f59bf6a58d0c9
Tabrez Rabbani <169131435+tabrez-tech-09@users.noreply.github.com>
2024-09-22 20:18:30 +0530

    2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
-const bar = document.getElementById('bar');
-const navbar = document.getElementById('navbar');
+// const bar = document.getElementById('bar');
+// const navbar = document.getElementById('navbar');
 
-if(bar) {
-    bar.addEventListener('click', () => {
-        navbar.classList.add('active');
-    })
-}
+// if(bar) {
+//     bar.addEventListener('click', () => {
+//         navbar.classList.add('active');
+//     })
+// }
+
+const bar = document.querySelector("#bar")
+const navbar1= document.querySelector("#navres")
+
+count = 0
+
+bar.addEventListener("click", function(){
+    if(count == 0){
+        navbar1.style.opacity = 1
+        count = 1
+    }
+
+    else if(count == 1){
+        navbar1.style.opacity= 0
+        count = 0
+    }
+
+})
