cb3357eca9068af914ecf3fdc2436122f9707171
Tabrez Rabbani <169131435+tabrez-tech-09@users.noreply.github.com>
2024-10-27 15:58:02 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
-const bar = document.getElementById('bar');
-const navbar = document.getElementById('navbar');
-
-if(bar) {
+ const bar = document.getElementById('bar');
+ const close = document.getElementById('close');
+ const nav = document.getElementById('navbar');
+ if(bar) {
     bar.addEventListener('click', () => {
-        navbar.classList.add('active');
+        nav.classList.add('active');
+     })
+ }
+ if(close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
     })
-}
+ }
